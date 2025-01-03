df09d9aedee8f3e9384455649757d3eb4e7b03d5
Adam soro <adamsoro0321@gmail.com>
2025-01-03 10:07:03 -0500

    v1.0

---
+++ emotion_detection.py
@@ -0,0 +1,14 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    """ 
+       
+    RETURN :
+       text
+    """
+    url: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    json: { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url,headers,json)
+    return response.text
