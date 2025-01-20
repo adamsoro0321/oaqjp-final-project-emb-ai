61f18a87f3a9fa5e3b28784135f87ce98aa1d3f0
Adam <adamsoro0321@gmail.com>
2025-01-20 16:43:28 -0500

    v2

--- emotion_detection.py
+++ emotion_detection.py
@@ -12,33 +12,34 @@ def emotion_detector(text_to_analyse):
         dict: A dictionary containing emotion scores and the dominant emotion.
     """
     # URL of the sentiment analysis service
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
     # Constructing the request payload in the expected format
     payload = {"raw_document": {"text": text_to_analyse}}
 
     # Custom header specifying the model ID for the sentiment analysis service
-    headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     # Sending a POST request to the sentiment analysis API
     response = requests.post(url, json=payload, headers=headers)
 
-    # Parsing the JSON response
+      # Converting the response text to a dictionary
     data = json.loads(response.text)
 
     # Extracting emotion scores
+    emotions = data.get("emotionPredictions", [{}])[0].get("emotion", {})
+
+    # Formatting the output dictionary
     emotion_scores = {
-        'anger': data.get('documentSentiment', {}).get('anger', 0),
-        'disgust': data.get('documentSentiment', {}).get('disgust', 0),
-        'fear': data.get('documentSentiment', {}).get('fear', 0),
-        'joy': data.get('documentSentiment', {}).get('joy', 0),
-        'sadness': data.get('documentSentiment', {}).get('sadness', 0),
+        'anger': emotions.get('anger', 0),
+        'disgust': emotions.get('disgust', 0),
+        'fear': emotions.get('fear', 0),
+        'joy': emotions.get('joy', 0),
+        'sadness': emotions.get('sadness', 0)
     }
 
-    # Determine the dominant emotion
+    # Determining the dominant emotion
     dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-
-    # Adding the dominant emotion to the result
     emotion_scores['dominant_emotion'] = dominant_emotion
 
     return emotion_scores
