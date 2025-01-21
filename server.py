29a48e36ec5deddcbc2d6986ff161136683ff0df
adamsoro0321 <adamsoro0321@example.com>
2025-01-21 09:50:55 +0000

    v3.2

--- server.py
+++ server.py
@@ -1,41 +1,41 @@
+"""
+Flask application for emotion detection using a sentiment analysis service.
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detector")
+# Create Flask application
+APP = Flask("EmotionDetector")
 
-
-@app.route("/emotionDetector")
+@APP.route("/emotionDetector")
 def detect_emotion():
     """
     Endpoint for detecting emotions in the provided text.
     Processes the text from the query parameter and returns the result.
     """
-    
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    # Check if the dominant emotion is None
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    # Format the output as a readable sentence
+    formatted_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}."
+    )
+    return formatted_response
 
-    if text_to_analyze:
-        # Pass the text to the emotion_detector function and store the response
-        response = emotion_detector(text_to_analyze)
-
-        # Format the output as a readable sentence
-        formatted_response = (
-            f"For the given statement, the system response is "
-            f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
-            f"'fear': {response['fear']}, 'joy': {response['joy']} and "
-            f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-        )
-        return formatted_response
-    else:
-        return "Error: Please provide text to analyze using the 'textToAnalyze' query parameter."
-
-@app.route("/")
+@APP.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    """
+    Renders the main application page.
+    """
     return render_template('index.html')
 
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    APP.run(host="127.0.0.1", port=5000)
